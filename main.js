295a229ff210b5323d3cd8b6b632fdd330ae53fc
chaima-syc <chaimaelomrani6@gmail.com>
2024-12-05 15:13:02 +0100

    practice la créaton popup form

---
+++ main.js
@@ -0,0 +1,26 @@
+// document.getElementById("LW").addEventListener("click", () => {
+//   document.querySelector(".popup-form-container").classList.remove("hidden");
+// });
+
+// document.querySelector(".popup-form-container").addEventListener("click", (e) => {
+//   if (e.target === e.currentTarget) {
+//     e.currentTarget.classList.add("hidden");
+//   }
+// });
+
+// document.getElementById("addPlayerForm").addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const newPlayer = {
+//     name: document.getElementById("playerName").value,
+//     position: document.getElementById("playerPosition").value,
+//     photo: document.getElementById("playerPhoto").value,
+//     nationality: document.getElementById("playerNationality").value,
+//     club: document.getElementById("playerClub").value,
+//     flag: document.getElementById("playerFlag").value,
+//     rating: document.getElementById("playerRating").value,
+//   };
+
+//   console.log("New Player Added:", newPlayer);
+//   document.querySelector(".popup-form-container").classList.add("hidden");
+// });
